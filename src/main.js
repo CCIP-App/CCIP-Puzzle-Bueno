69bf103d92e6d5d4a8e0211e0cdf1c16e65df12e
Erickson Juang <a820224@gmail.com>
2017-07-04 01:26:57 +0800

    Remove vuetify.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify'
 import store from './store/index'
 import router from './router/index'
 import { sync } from 'vuex-router-sync'
 import Components from './components/_index'
 
 sync(store, router)
-
-Vue.use(Vuetify)
 
 Object.keys(Components).forEach(key => {
   Vue.component(key, Components[key])
